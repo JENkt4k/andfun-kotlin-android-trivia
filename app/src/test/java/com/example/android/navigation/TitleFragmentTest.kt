fcdd0e10264a7aba9fe45a4f7cabc27902ff2620
James Nelson <james.e.nelson@gmail.com>
2019-12-15 17:10:23 -0600

    Working step 9: - added navigation, nav onlcick listener, nav fragment - added unit tests

---
+++ app/src/test/java/com/example/android/navigation/TitleFragmentTest.kt
@@ -0,0 +1,17 @@
+package com.example.android.navigation
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class TitleFragmentTest {//extends GroovyTestCase {
+@Test
+fun addition_isCorrect() {
+    assertEquals(4, 2 + 2)
+}
+
+    @Test
+    fun subtraction_isCorrect() {
+        assertEquals(0, 2 - 2)
+    }
+}
